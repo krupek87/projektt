d83dada6c985f8c8e91f4a689728c6144049a843
Pawel Krupicki <krupek87@wp.pl>
2017-06-09 13:45:21 +0200

    dasdas
    dasdasd

---
+++ scripts.js
@@ -0,0 +1,57 @@
+function Shape(sideLengths) {
+    this._name = "";
+    this._sideLengths = sideLengths;
+}
+
+Shape.prototype.getPerimeter = function() {
+
+    return this._sideLengths.reduce(function(prevVal, val) {
+        return prevVal + val;
+    });
+
+};
+
+Shape.prototype.getArea = function() {
+    return this._sideLengths[0] * this._sideLengths[1];
+};
+
+
+function Rectangle(sideLengths) {
+    Shape.call(this, [sideLengths[0], sideLengths[1], sideLengths[0], sideLengths[1]]);
+
+    this._name = "Prostokąt";
+}
+
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+function Square(sideLength) {
+    Rectangle.call(this, [sideLength, sideLength]);
+
+    this._name = "Kwadrat";
+}
+
+Square.prototype = Object.create(Rectangle.prototype);
+Square.prototype.constructor = Square;
+
+function Triangle(sideLength) {
+    Shape.call(this, [sideLength, sideLength, sideLength]);
+
+    this._name = "Trojkat";
+
+
+}
+
+Triangle.prototype = Object.create(Shape.prototype);
+Triangle.prototype.constructor = Triangle;
+
+ Triangle.prototype.getArea = function(){
+
+    	var a = this._sideLengths[0];
+    	
+    	return +( (a * a * Math.sqrt(3)) / 4).toFixed(2);
+    }
+
+var shape1 = new Triangle(10);
+
+
